b7b4231f9ff8b7c6a597a30fa44cb09e41a5c0c7
Jamie Greeff <jamie@greeff.me>
2024-08-16 12:45:29 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
